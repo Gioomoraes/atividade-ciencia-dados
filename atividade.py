680136f73c19252b7fe44ab37c2b7bbfb445dee4
Giovanna Moraes <160680690+Gioomoraes@users.noreply.github.com>
2026-04-08 16:24:29 -0300

    Exemplo de Loop, dicionário e lista

---
+++ atividade.py
@@ -0,0 +1,12 @@
+for counter in range(5):
+    print(counter)
+
+print("----")
+
+example_dict = {'apples': 5, 'oranges': 8, 'bananas': 13}
+print(example_dict)
+
+print("----")
+
+dict_to_list = list(example_dict)
+print(dict_to_list)
